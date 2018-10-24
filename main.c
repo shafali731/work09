880b54549a782ebbb74d221b9a6b9caea09f0d60
Shafalil Gupta <sgupta@stuy.edu>
2018-10-24 08:07:31 -0400

    added

--- main.c
+++ main.c
@@ -1,36 +1,35 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <fcntl.h> //for open
 #include <unistd.h> //for close
 #include <string.h>
+int main(){
+  char c[128];
+  char *name = "foo.txt";
+  int fd = open(name, O_WRONLY);
+  printf("fd = %d\n", fd);
+  if (fd < 0)
+    printf("File already exists. File not created or opened.\n");
 
-int main() {
-  //OPEN
-  //if file not in directory, foo.txt created
-  int fd = open("foo.txt", O_RDWR | O_CREAT);  
-  printf("fd = %d\n", fd); 
-  if (fd < 0) 
-    printf("File already exists. File not created or opened.\n");           
-
-  //CLOSE 
-  //using close system call 
+  char added[] = "hello friends";
+  write(fd, added, 128);
+  printf("hello friends was written\n");
   if (close(fd) < 0) {
-    printf("File already exists. File not closed.\n"); 
-    exit(1); 
-  }  
+    printf("File already exists. File not closed.\n");
+    exit(1);
+  }
   printf("closed the fd\n");
 
-  //WRITE
-  int fd1 = open("foo.txt", O_RDWR);
-  int sz = write(fd1, "hello friends\n", strlen("hello friends\n"));
-  printf("hello friends was written\n");
-  
-  //READ
-  char *c = (char *) calloc(100, sizeof(char));
-  sz = read(fd1, c, 10);
-  printf("called read(% d, c, 10).  returned that"
-         " %d bytes  were read.\n", fd1, sz); 
-  c[sz] = '\0'; 
-  printf("Those bytes are as follows: %s\n", c); 
+  int fd1 = open(name, O_RDWR);
+  int sz= read(fd1, c, strlen("hello friends\n"));
+  printf("bytes read: %d\n", sz);
+
+  printf("Those bytes are as follows: %s\n", c);
+
+  //printf("'%s'\n", c);
+  printf("Closing '%s'\n", name);
   close(fd1);
-} 
+  return 0;
+}
