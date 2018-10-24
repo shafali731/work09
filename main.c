91efca9d0bf44f4eb75d0f1c2596a4c7bec4da6a
ennylgnaw <lwang7@stuy.edu>
2018-10-24 04:20:49 -0400

    Added write and read

--- main.c
+++ main.c
@@ -2,22 +2,35 @@
 #include <stdlib.h>
 #include <fcntl.h> //for open
 #include <unistd.h> //for close
+#include <string.h>
 
 int main() {
   //OPEN
   //if file not in directory, foo.txt created
-  int fd = open("foo.txt", O_RDONLY | O_CREAT);  
+  int fd = open("foo.txt", O_RDWR | O_CREAT);  
   printf("fd = %d\n", fd); 
   if (fd < 0) 
     printf("File already exists. File not created or opened.\n");           
 
   //CLOSE 
-  // Using close system call 
+  //using close system call 
   if (close(fd) < 0) {
     printf("File already exists. File not closed.\n"); 
     exit(1); 
   }  
   printf("closed the fd\n");
 
+  //WRITE
+  int fd1 = open("foo.txt", O_RDWR);
+  int sz = write(fd1, "hello friends\n", strlen("hello friends\n"));
+  printf("hello friends was written\n");
+  
   //READ
+  char *c = (char *) calloc(100, sizeof(char));
+  sz = read(fd1, c, 10);
+  printf("called read(% d, c, 10).  returned that"
+         " %d bytes  were read.\n", fd1, sz); 
+  c[sz] = '\0'; 
+  printf("Those bytes are as follows: %s\n", c); 
+  close(fd1);
 } 
